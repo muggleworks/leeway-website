d03a713c35b03fd4d1ef383248b280075f321a02
Rishiraj <rishirajt21@gmail.com>
2023-04-19 18:54:47 +0530

    changes

--- public/app.js
+++ public/app.js
@@ -1,26 +1,25 @@
 let lastId = null;
 let lastId1 = null;
-function expand(ElementId, ElementId1) {
-
-    let imgElement = document.getElementById(ElementId1);
+function expand(elementId) {
+    let imgElement = document.getElementById(`${elementId}-icon`);
     imageSrc = imgElement.src;
     imageSrc = imageSrc.split("/").pop();
     imageSrc = (imageSrc === "minus.svg") ? "images/add.svg" : "images/minus.svg";
     imgElement.src = imageSrc;
 
-    if (lastId && lastId != ElementId) {
+    if (lastId && lastId != elementId) {
         document.getElementById(lastId).classList.add("hidden");
 
     }
-    if (lastId1 && lastId1 != ElementId1) {
+    if (lastId1 && lastId1 != (`${elementId}-icon`)) {
         document.getElementById(lastId1).src = "images/add.svg";
     }
 
 
-    const content = document.getElementById(ElementId);
+    const content = document.getElementById(elementId);
     content.classList.toggle("hidden");
-    lastId = ElementId;
-    lastId1 = ElementId1;
+    lastId = elementId;
+    lastId1 = (`${elementId}-icon`);
 
 }
 
